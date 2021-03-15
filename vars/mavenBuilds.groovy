cc761a7be6c71fafcb6f69884f65fc54aaf4de70
jenkins <thomas@mosig-frey.de>
2021-03-15 19:03:11 +0100

    jenkinslib: init

---
+++ vars/mavenBuilds.groovy
@@ -0,0 +1,52 @@
+import com.jenkins.*;
+
+def call(body) {
+    LinkedHashMap config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipeline {
+
+        tools {
+            jdk 'linux_jdk8u221'
+            maven 'linux_M3'
+        }
+        agent any
+
+        stages {
+            stage('Compile/Test/Install') {
+                steps {
+                    withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
+                        sh 'mvn clean install'
+                    }
+                }
+            }
+
+            stage('Code Analysis') {
+                steps {
+                    withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
+                        withSonarQubeEnv('jenkins') {
+                            sh 'mvn sonar:sonar'
+                        }
+                    }
+                }
+            }
+
+            stage('Deploy') {
+                steps {
+                    withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
+                        sh 'echo mvn deploy -Dmaven.test.skip=true'
+                    }
+                }
+            }
+
+            stage('Archive artifacts') {
+                steps {
+                    archiveArtifacts artifacts: '**/*.jar',  allowEmptyArchive: true
+                    archiveArtifacts artifacts: 'target/surefire-reports/*.xml',  allowEmptyArchive: true
+                }
+            }
+        }
+    }
+}
