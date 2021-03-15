f51d3a92044f9b52e0fa99aa0072380cf2426f9d
jenkins <thomas@mosig-frey.de>
2021-03-15 19:37:39 +0100

    jenkinslib: init

---
+++ vars/testPipeLine.groovy
@@ -0,0 +1,86 @@
+import com.jenkins.*;
+
+def call(body) {
+    LinkedHashMap config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipeline {
+
+        agent { label 'mac'}
+
+        parameters {
+            choice(name: 'ACTION', choices: ['Compile', 'Test', 'Deploy'], description: 'Pls select action to do:')
+        }
+
+        stages {
+
+            stage ('parallel build/test/push') {
+                parallel {
+                    stage('MAC Build (Java Threading)') {
+                        agent { label 'mac' }
+                        tools {
+                            jdk 'linux_jdk1.8.0_172'
+                            maven 'linux_M3'
+                        }
+                        steps {
+                            // checkout
+                            git branch: 'master', credentialsId: 'd6ae3020-ebc8-4fa7-9071-5f96e10ce3f8', url: 'https://thomasmosigfrey@git.code.sf.net/p/threadingexample/code'
+
+                            // build
+                            withMaven(jdk: 'linux_jdk1.8.0_172', maven: 'linux_M3') {
+                                sh 'mvn clean install'
+                            }
+
+                            // tests
+                            withMaven(jdk: 'linux_jdk1.8.0_172', maven: 'linux_M3') {
+                                sh 'mvn test'
+                            }
+                            // archive artifacts
+                            stash allowEmpty: true, includes: 'target/Thr*.jar', name: 'jarFilesThreading'
+                        }
+                    }
+                    stage('UNIX Build (Kotlin) ') {
+                        agent { label 'unix' }
+                        tools {
+                            jdk 'linux_jdk1.8.0_172'
+                            maven 'linux_M3'
+                        }
+                        steps {
+                            // checkout
+                            git branch: 'master', credentialsId: 'd6ae3020-ebc8-4fa7-9071-5f96e10ce3f8', url: 'https://thomasmosigfrey@git.code.sf.net/p/the-example-app-kotlin/code'
+
+                            // build
+                            withMaven(jdk: 'linux_jdk1.8.0_172', maven: 'linux_M3') {
+                                sh 'mvn clean install'
+                            }
+
+                            // tests
+                            withMaven(jdk: 'linux_jdk1.8.0_172', maven: 'linux_M3') {
+                                sh 'mvn test'
+                            }
+                            // archive artifacts
+                            stash allowEmpty: true, includes: 'target/dagg*.jar', name: 'jarFilesKotlin'
+                        }
+                    }
+                }
+            }
+            stage ('SIT Test') {
+                agent { label 'test' }
+                tools {
+                    jdk 'linux_jdk1.8.0_172'
+                    maven 'linux_M3'
+                }
+                steps {
+                    // retrieve artifacts
+                    unstash 'jarFilesKotlin'
+                    unstash 'jarFilesThreading'
+
+                    // use them testwise
+                    // save test results
+                }
+            }
+        }
+    }
+}
