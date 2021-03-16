bdf63e2e406a5971c6ac0b2ce1bfeb2acb66bbe1
jenkins <thomas@mosig-frey.de>
2021-03-16 13:24:51 +0100

    jenkinslib: init

--- src/com/jenkins/MavenBuild.groovy
+++ src/com/jenkins/MavenBuild.groovy
@@ -4,10 +4,10 @@ import com.jenkins.lib.CallCounter;
 import hudson.*;
 
 public class MavenBuild {
-    static int cleanInstall(def steps) {
+    static void cleanInstall(def steps) {
         steps.withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
+            steps.echo "CallCounter: " + CallCounter.count()
             steps.sh 'mvn clean install'
         }
-        return CallCounter.count()
     }
 }
