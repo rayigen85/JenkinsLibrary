fbb4468b890b77c2afffcf8d408d1d1648462d02
jenkins <thomas@mosig-frey.de>
2021-03-16 11:29:40 +0100

    jenkinslib: init

--- src/com/jenkins/mavenBuild.groovy
+++ src/com/jenkins/mavenBuild.groovy
@@ -1,9 +1,11 @@
 import com.jenkins.lib.*;
 
-int cleanInstall() {
-    withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
-        sh 'mvn clean install'
-        echo config.test
+class mavenBuild {
+    int cleanInstall() {
+        withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
+            sh 'mvn clean install'
+            echo config.test
+        }
+        return CallCounter.count()
     }
-    return CallCounter.count()
 }
