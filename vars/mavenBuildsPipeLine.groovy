5b85c515f6ce6adcf7f30656ce50ab0735437480
jenkins <thomas@mosig-frey.de>
2021-03-16 15:38:00 +0100

    jenkinslib: init

--- vars/mavenBuildsPipeLine.groovy
+++ vars/mavenBuildsPipeLine.groovy
@@ -12,84 +12,83 @@ def call(body) {
     body.delegate = config
     body()
 
-    lock(env.BRANCH_NAME) {
+    pipeline {
 
-        pipeline {
+        tools {
+            jdk 'linux_jdk8u221'
+            maven 'linux_M3'
+        }
 
-            tools {
-                jdk 'linux_jdk8u221'
-                maven 'linux_M3'
+        triggers {
+            cron(config.cron)
+        }
+
+        agent any
+
+        environment {
+            MYKEY = "value"
+        }
+
+        stages {
+
+            stage('Print build Variables') {
+                steps {
+                    echo currentBuild.buildVariables.MYKEY
+                }
             }
 
-            triggers {
-                cron(config.cron)
-            }
-
-            agent any
-
-            environment {
-                MYKEY = "value"
-            }
-
-            stages {
-
-                stage('Print build Variables') {
-                    steps {
-                        echo currentBuild.buildVariables.MYKEY
+            stage('Print previous build Variables') {
+                when {
+                    not {
+                        equals expected: null, actual: currentBuild.previousBuild
                     }
                 }
-
-                stage('Print previous build Variables') {
-                    when {
-                        not {
-                            equals expected: null, actual: currentBuild.previousBuild
-                        }
-                    }
-                    steps {
-                        echo currentBuild.previousBuild.buildVariables.MYKEY
-                    }
+                steps {
+                    echo currentBuild.previousBuild.buildVariables.MYKEY
                 }
+            }
 
-                stage('Compile/Test/Install') {
+            stage('Compile/Test/Install') {
+                lock(env.BRANCH_NAME) {
                     steps {
                         script {
                             cleanInstall(this)
                         }
                     }
                 }
+            }
 
-                stage('Code Analysis') {
-                    steps {
-                        withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
-                            withSonarQubeEnv('jenkins') {
-                                script {
-                                    try {
-                                        sh 'mvn sonar:sonar'
-                                    } catch (exc) {
-                                        echo 'sonar checks failed: ' + exc.message
-                                        //throw new hudson.AbortException("sonar checks failed: " + exc.message)
-                                    } finally {
-                                        echo 'sonar step finished'
-                                    }
+            stage('Code Analysis') {
+                steps {
+                    withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
+                        withSonarQubeEnv('jenkins') {
+                            script {
+                                try {
+                                    sh 'mvn sonar:sonar'
+                                } catch (exc) {
+                                    echo 'sonar checks failed: ' + exc.message
+                                    //throw new hudson.AbortException("sonar checks failed: " + exc.message)
+                                } finally {
+                                    echo 'sonar step finished'
                                 }
                             }
                         }
                     }
                 }
+            }
 
-                stage('Deploy') {
-                    steps {
-                        withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
-                            sh 'echo mvn deploy -Dmaven.test.skip=true'
-                        }
+            stage('Deploy') {
+                steps {
+                    withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
+                        sh 'echo mvn deploy -Dmaven.test.skip=true'
                     }
                 }
+            }
 
-                stage('Archive artifacts') {
-                    steps {
-                        archiveArtifacts artifacts: '**/*.jar', allowEmptyArchive: true
-                        archiveArtifacts artifacts: 'target/surefire-reports/*.xml', allowEmptyArchive: true
-                    }
+            stage('Archive artifacts') {
+                steps {
+                    archiveArtifacts artifacts: '**/*.jar', allowEmptyArchive: true
+                    archiveArtifacts artifacts: 'target/surefire-reports/*.xml', allowEmptyArchive: true
                 }
             }
         }
