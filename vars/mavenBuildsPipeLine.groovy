42c6eb89cf05c3c95595d2c684fab8545c9fd7b4
jenkins <thomas@mosig-frey.de>
2021-03-16 10:49:46 +0100

    jenkinslib: init

--- vars/mavenBuildsPipeLine.groovy
+++ vars/mavenBuildsPipeLine.groovy
@@ -24,6 +24,7 @@ def call(body) {
                 steps {
                     withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
                         sh 'mvn clean install'
+                        echo config.test
                     }
                 }
             }
