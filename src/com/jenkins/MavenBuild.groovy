e9b0423526031e1a7ab53985478f7bb136970e12
jenkins <thomas@mosig-frey.de>
2021-03-16 12:54:03 +0100

    jenkinslib: init

--- src/com/jenkins/MavenBuild.groovy
+++ src/com/jenkins/MavenBuild.groovy
@@ -7,7 +7,6 @@ public class MavenBuild {
     static int cleanInstall(def steps) {
         steps.withMaven(jdk: 'linux_jdk8u221', maven: 'linux_M3') {
             steps.sh 'mvn clean install'
-            steps.echo config.test
         }
         return CallCounter.count()
     }
