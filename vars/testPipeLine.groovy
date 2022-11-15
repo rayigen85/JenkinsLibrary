b1e2385ea7b40070af88fcf35b22b2ea8f4e17e9
rayigen85 <raymondbaruth@raybit.de>
2022-11-15 12:55:55 +0100

    Update testPipeLine.groovy

--- vars/testPipeLine.groovy
+++ vars/testPipeLine.groovy
@@ -6,69 +6,48 @@ def call(body) {
     body.delegate = config
     body()
 
-    pipeline {
-
-        agent { label 'mac'}
-
-        stages {
-
-            stage ('parallel build/test/push') {
-                parallel {
-                    stage('MAC Build (Java Threading)') {
-                        agent { label 'mac' }
-                        tools {
-                            maven 'Maven 3.3.9'
-                        }
-                        steps {
-                            dir('threading') {
-                                // checkout
-                                git branch: 'master', credentialsId: 'd6ae3020-ebc8-4fa7-9071-5f96e10ce3f8', url: 'https://thomasmosigfrey@git.code.sf.net/p/threadingexample/code'
-
-                                // build
-                                withMaven( maven: 'Maven 3.3.9') {
-                                    sh 'mvn clean install'
-                                }
-
-                                // archive artifacts
-                                stash allowEmpty: true, includes: 'target/Thr*.jar', name: 'jarFilesThreading'
-                            }
-                        }
-                    }
-                    stage('UNIX Build (Kotlin) ') {
-                        agent { label 'unix' }
-                        tools {
-                            maven 'Maven 3.3.9'
-                        }
-                        steps {
-                            dir('kotlin') {
-                                // checkout
-                                git branch: 'master', credentialsId: 'd6ae3020-ebc8-4fa7-9071-5f96e10ce3f8', url: 'https://thomasmosigfrey@git.code.sf.net/p/the-example-app-kotlin/code'
-
-                                // build
-                                withMaven(maven: 'Maven 3.3.9') {
-                                    sh 'mvn clean install'
-                                }
-
-                                // archive artifacts
-                                stash allowEmpty: true, includes: 'target/dagg*.jar', name: 'jarFilesKotlin'
-                            }
-                        }
-                    }
+   pipeline {
+    agent {
+        label 'rbaruth'
+    }
+    stages {
+        stage('WS cleanup'){
+            steps{cleanWs()}
+            
+        }
+        
+        stage('SCM checkout') {
+            steps {
+                git credentialsId: '1743a11b-ed27-416c-a3b9-de0a1555a33f', url: 'https://github.com/ThomasMosigFrey/jee8.git/'
+                }
+        }
+        stage('compile/test') {
+            steps {
+                withMaven(maven: 'Maven 3.8.6') {
+                    sh "mvn clean package"
                 }
             }
-            stage ('SIT Test') {
-                agent { label 'test' }
-                tools {
-                    maven 'Maven 3.3.9'
-                }
-                steps {
-                    // retrieve artifacts
-                    unstash 'jarFilesKotlin'
-                    unstash 'jarFilesThreading'
-
-                    // use them testwise
-                    // save test results
-                }
+        }
+        stage('install') {
+            steps {
+                sh '''
+                    echo install
+                '''
+            }
+        }
+        stage('publish') {
+            steps {
+                sh '''
+                    echo publish
+                '''
+            }
+        }
+        stage('report') {
+            steps {
+                echo 'report'
+                sh '''
+                    echo report
+                '''
             }
         }
     }
