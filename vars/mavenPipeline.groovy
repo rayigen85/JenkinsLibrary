4baf975b45c926d7b3c1f00d60bca242c00988d1
ThomasMosigFrey <43186033+ThomasMosigFrey@users.noreply.github.com>
2022-11-15 11:15:50 +0100

    Create mavenPipeline.groovy

---
+++ vars/mavenPipeline.groovy
@@ -0,0 +1,51 @@
+import com.jenkins.*;
+
+def call(body) {
+    LinkedHashMap config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    pipeline {
+      agent { label 'Demo' }
+      stages {
+          stage('Compile/Test') {
+              steps {
+                  withMaven(maven: 'Maven 3.8.6') {
+                      sh "mvn clean install"
+                  }
+              }
+          }
+          stage('Report') {
+              steps {
+                  junit allowEmptyResults: true, testResults: '**/*.xml'
+              }
+          }
+          stage('Source Code Check') {
+              steps {
+                  withMaven(maven: 'Maven 3.8.6') {
+                      withSonarQubeEnv(credentialsId: 'e33b1bc1-7530-4989-b282-d348ac73bc27', installationName: 'sonar') { // You can override the credential to be used
+                          sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+                      }
+                  }
+              }
+          }
+          stage('Deploy') {
+              steps {
+                  withMaven(maven: 'Maven 3.8.6') {
+                      echo "deploy"
+                  }
+              }
+          }
+     }
+     post {
+         always {
+             sh 'printenv'
+             emailext body: '', recipientProviders: [developers()], subject: 'Build executed ', to: 'thomas@mosig-frey.de'
+         }
+         success {
+              cleanWs()
+         }
+     }
+  }
+}
